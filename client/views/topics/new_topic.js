389c475a22ef5bcf6b672479694e3ffd705033fc
Wei Ouyang <oeway007@gmail.com>
2015-10-20 01:11:10 +0200

    add kandle bookmarklet and REST api

--- client/views/topics/new_topic.js
+++ client/views/topics/new_topic.js
@@ -1,13 +1,8 @@
-Template.newTopic.events({
-  'submit #js-create-topic-form': function(event, template) {
-    event.preventDefault();
-
-    var $title = template.$('#js-create-title');
-    var $description = template.$('#js-create-description');
+var createNewTopic = function($title,$description){
 
     var topic = {
-      title: $title.val(),
-      description: $description.val()
+      title: $title,
+      description: $description
     };
 
     Meteor.call('newTopic', topic, function (error, result) {
@@ -19,7 +14,13 @@ Template.newTopic.events({
         else if (error.error === 'invalid-content')
           toastr.warning(i18n.t('topic_too_short'));
         else if (error.error === 'duplicate-content')
-          toastr.warning(i18n.t('topic_title_exists'));
+        {
+            toastr.warning(i18n.t('topic_title_exists'));
+            $('#one-modal').modal('hide').on('hidden.bs.modal', function () {
+                //Router.go('topic', { '_id': result });  //TODO: urgly hack
+            });
+            Router.go('topic', { '_id': error.details });
+        }
         else
           toastr.warning(i18n.t('error'));
       }
@@ -28,9 +29,42 @@ Template.newTopic.events({
         $description.val('');
 
         $('#one-modal').modal('hide').on('hidden.bs.modal', function () {
-          Router.go('topic', { '_id': result });
+          //Router.go('topic', { '_id': result });  //TODO: urgly hack
         });
+        Router.go('topic', { '_id': result });
       }
     });
+}
+
+Template.newTopic.events({
+  'submit #js-create-topic-form': function(event, template) {
+    event.preventDefault();
+    var $title = template.$('#js-create-title').val();
+    var $description = template.$('#js-create-description').val();
+    createNewTopic($title,$description);
+
   }
+});
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+Template.newTopic.helpers({
+  new_topic_title:function(){
+      return getParameterByName("text");
+  },
+  new_topic_description:function(){
+      return getParameterByName("comment");
+  }
+});
+
+Template.newTopic.onRendered(function () {
+    if(getParameterByName("text")!="")
+    {
+        createNewTopic(getParameterByName("text"),getParameterByName("comment"));
+    }
 });
